f9046b2040e5770365b2223004c2caf04494451d
taljacob2 <70590583+taljacob2@users.noreply.github.com>
2021-03-01 18:12:58 +0200

    Add files via upload

---
+++ Java_Decompiler/decompile.bat
@@ -0,0 +1,1 @@
+java -cp java-decompiler.jar org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -dgs=true %cd%/classesFolder %cd%/decompileFolder
---
+++ Java_Decompiler/decompile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -cp java-decompiler.jar org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -dgs=true ./classesFolder ./decompileFolder
+
